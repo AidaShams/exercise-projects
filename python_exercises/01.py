ec1ebc6d9562d31c8195db3a84473a05d7af1b88
Aida Shams <aidashmas77@gmail.com>
2025-02-10 21:33:07 -0800

    "for" python exercise

--- python_exercises/01.py
+++ python_exercises/01.py
@@ -1,8 +1,7 @@
 #iterable = (1,2,3,4,5)
-#i=2
 #for i in iterable:
 #    print(i)
-#--------------------------------
+#----------------------------------
 # writes hi for each character in "iterable"
 #for i in "iterable":
 #    print("hi")
@@ -10,3 +9,22 @@
 # when you dont wanna use the name of the variable inside the loop:
 #for _ in "welcome":
 #    print("hi")
+#----------------------------------
+#for i in str(100):     #for i in "100"
+#    print(type(int(i)), ":", int(i))
+#----------------------------------
+# from 1 to 10 (we write 11 cuz it stops before 10)
+#for i in range(1, 11,1):
+#    print(i)
+#----------------------------------
+          #محور i,j
+#i= 1, 2, 3
+#j= 10, 20, 30, 40, 50, 60
+for i in range(1,4):
+    for j in range(10, 61, 10):
+        print(i, ":", j)
+#first holds the same i for all js and print them then goes for the outside for and goes to the next i and so on.
+#----------------------------------
+
+
+
