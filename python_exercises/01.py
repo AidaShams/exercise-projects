e1d01dcaa08900847afab6003059999b70c847a3
Aida Shams <aidashmas77@gmail.com>
2025-02-10 21:07:45 -0800

    "for" python exercise

--- python_exercises/01.py
+++ python_exercises/01.py
@@ -1,4 +1,7 @@
-iterable = (1,2,3,4,5)
+#iterable = (1,2,3,4,5)
 #i=2
-for i in iterable:
-    print(i)
+#for i in iterable:
+#    print(i)
+#--------------------------------
+for i in "iterable":
+    print("hi")
